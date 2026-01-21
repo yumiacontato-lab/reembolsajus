dc8ee1275c062df01f18406b43c24644394468ee
danilocostaa11 <danilocostaa11@gmail.com>
2026-01-21 13:31:35 -0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig(({ mode }) => {
   return {
     server: {
       host: "0.0.0.0",
-      port: Number(process.env.PORT) || 5000,
+      port: Number(process.env.PORT) || 5001,
       allowedHosts: true,
     },
     plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
